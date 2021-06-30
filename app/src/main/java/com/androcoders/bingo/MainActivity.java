229864794921703bc0304f9b1677d8f8667a6179
kirthi <kirthi.krishnaveni@gmail.com>
2021-06-30 12:01:59 +0530

    splash screen

--- app/src/main/java/com/androcoders/bingo/MainActivity.java
+++ app/src/main/java/com/androcoders/bingo/MainActivity.java
@@ -2,13 +2,40 @@ package com.androcoders.bingo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
 public class MainActivity extends AppCompatActivity {
+    private TimerTask task;
+    Intent intent;
+    private Timer time;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        getSupportActionBar().hide();
+        time=new Timer();
+        intent=new Intent();
+
     }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        task=new TimerTask() {
+            @Override
+            public void run() {
+          intent.setClass(getApplicationContext(),Homepage.class);
+          startActivity(intent);
+          finish();
+            }
+        };
+        time.schedule(task,2000);
+        }
 }
