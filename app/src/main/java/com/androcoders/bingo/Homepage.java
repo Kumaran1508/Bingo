bf2df7b728989bcb17eb66a8bc1f56b8f99a62d0
sanjaykumaroffl <sanjaykumaroffl@gmail.com>
2021-07-01 13:33:25 +0530

    recycler item

--- app/src/main/java/com/androcoders/bingo/Homepage.java
+++ app/src/main/java/com/androcoders/bingo/Homepage.java
@@ -2,15 +2,29 @@ package com.androcoders.bingo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 public class Homepage extends AppCompatActivity {
+
+    private Button create;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_homepage);
+
+        create=findViewById(R.id.create_btn);
+
+        create.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent().setClass(getApplicationContext(),CreateRoom.class));
+            }
+        });
 
         getSupportActionBar().hide();
         Toast.makeText(getApplicationContext(), ""+getIntent().getStringExtra("name"), Toast.LENGTH_SHORT).show();
