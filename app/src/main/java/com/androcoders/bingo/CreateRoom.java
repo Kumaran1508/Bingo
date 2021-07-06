8e830a7fd305830adc65dc62b7639a2e3fbd2e02
Kumaran <kumarans1508@gmail.com>
2021-07-06 13:44:11 +0530

    Room Generated

--- app/src/main/java/com/androcoders/bingo/CreateRoom.java
+++ app/src/main/java/com/androcoders/bingo/CreateRoom.java
@@ -1,6 +1,7 @@
 package com.androcoders.bingo;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
@@ -17,6 +18,7 @@ import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QuerySnapshot;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Random;
 
@@ -27,6 +29,9 @@ public class CreateRoom extends AppCompatActivity {
     private Button start;
     private FirebaseAuth auth= FirebaseAuth.getInstance();
     private FirebaseFirestore firebaseFirestore= FirebaseFirestore.getInstance();
+    private boolean uniquekey = false;
+    private int roomkey = 0;
+    private ArrayList<Player> players_list=new ArrayList<>();
 
 
     @Override
@@ -40,44 +45,54 @@ public class CreateRoom extends AppCompatActivity {
 
         Toast.makeText(this, ""+getIntent().getStringExtra("id"), Toast.LENGTH_SHORT).show();
 
-        int roomkey = generateroom();
+        PlayerAdaptor adaptor = new PlayerAdaptor(getApplicationContext(),players_list);
+        players.setLayoutManager(new LinearLayoutManager(getApplicationContext()));
+        players.setAdapter(adaptor);
+        players.setHasFixedSize(true);
+    }
 
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        generateroom();
+    }
+
+    void createRoom(){
         GoogleSignInAccount account = GoogleSignIn.getLastSignedInAccount(getApplicationContext());
-
         HashMap<String,Object> room = new HashMap<>();
         room.put("isStarted",false);
         room.put("owner",account.getId());
 
-
         firebaseFirestore.collection("rooms").document(String.valueOf(roomkey)).set(room);
-
     }
+
     int generatekey() {
         Random random= new Random();
         int min= 10000;
         int max= 99999;
-        int roomkey=random.nextInt((max - min) + 1) + min;
-        return roomkey;
+        int key=random.nextInt((max - min) + 1) + min;
+        return key;
 
     }
-    int generateroom() {
-        int roomkey=generatekey();
-        final boolean[] uniquekey = {false};
-
+    void generateroom() {
         firebaseFirestore.collection("rooms").get().addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
             @Override
             public void onSuccess(QuerySnapshot queryDocumentSnapshots) {
-                for (DocumentSnapshot document : queryDocumentSnapshots){
-                    int id= Integer.valueOf(document.getId());
-                    if (id==roomkey){
-                        return;
+                roomkey = generatekey();
+                while(!uniquekey) {
+                    for (DocumentSnapshot document : queryDocumentSnapshots) {
+                        int id = Integer.valueOf(document.getId());
+                        if (id == roomkey) {
+                            roomkey = generatekey();
+                            return;
+                        }
                     }
+                    uniquekey = true;
+                    room_id.setText(room_id.getText().toString()+"\n"+String.valueOf(roomkey));
+                    createRoom();
                 }
-                uniquekey[0] = true;
             }
         });
-        if (uniquekey[0])
-            return roomkey;
-        return generateroom();
     }
 }
