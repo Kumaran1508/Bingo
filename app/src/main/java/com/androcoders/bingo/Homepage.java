c389496692db6adcfd860c956a5b45461be763b2
sanjaykumaroffl <sanjaykumaroffl@gmail.com>
2021-07-06 12:31:28 +0530

    login completed final

--- app/src/main/java/com/androcoders/bingo/Homepage.java
+++ app/src/main/java/com/androcoders/bingo/Homepage.java
@@ -8,9 +8,15 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.firebase.auth.FirebaseAuth;
+
 public class Homepage extends AppCompatActivity {
 
     private Button create;
+    private Button logout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,12 +24,33 @@ public class Homepage extends AppCompatActivity {
         setContentView(R.layout.activity_homepage);
 
         create=findViewById(R.id.create_btn);
+        logout=findViewById(R.id.logout_btn);
 
         create.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 //Intent intent=new Intent()
                 startActivity(getIntent().setClass(getApplicationContext(),CreateRoom.class));
+            }
+        });
+
+        logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent= new Intent();
+                intent.setClass(getApplicationContext(),Loginpage.class);
+                intent.putExtra("auto",false);
+                FirebaseAuth auth= FirebaseAuth.getInstance();
+                GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                        .requestEmail()
+                        .requestId()
+                        .requestProfile()
+                        .build();
+
+                GoogleSignInClient user = GoogleSignIn.getClient(getApplicationContext(),gso);
+                user.signOut();
+                startActivity(intent);
+                finish();
             }
         });
 
