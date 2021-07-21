04a393bb578f95e48492cad3828a3d93b2e92bed
Kumaran <kumarans1508@gmail.com>
2021-07-21 13:47:04 +0530

    crash fixedddddd 100%

--- app/src/main/java/com/androcoders/bingo/PlayerAdaptor.java
+++ app/src/main/java/com/androcoders/bingo/PlayerAdaptor.java
@@ -6,6 +6,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
@@ -30,13 +31,17 @@ public class PlayerAdaptor extends RecyclerView.Adapter<PlayerAdaptor.ViewHolder
         this.isOwner=isOwner;
         this.roomkey=roomkey;
 
-        FirebaseFirestore.getInstance().collection("rooms").document(roomkey)
-                .get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
-            @Override
-            public void onSuccess(DocumentSnapshot documentSnapshot) {
-                ownerId = documentSnapshot.getString("owner");
-            }
-        });
+        try{
+            FirebaseFirestore.getInstance().collection("rooms").document(roomkey)
+                    .get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+                @Override
+                public void onSuccess(DocumentSnapshot documentSnapshot) {
+                    ownerId = documentSnapshot.getString("owner");
+                }
+            });
+        }catch (Exception e){
+            Toast.makeText(context, "error retreiving owner name", Toast.LENGTH_SHORT).show();
+        }
 
     }
 
